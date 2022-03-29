2ceaa7da46ac1c095464760937ded6197f67f9d9
andrewelamb <andrewelamb@gmail.com>
2022-03-29 09:49:48 -0700

    instal venv in workflow

--- R/global.R
+++ R/global.R
@@ -1,5 +1,5 @@
 # Activate virtual env
-#Sys.unsetenv("RETICULATE_PYTHON")
+Sys.unsetenv("RETICULATE_PYTHON")
 # Note, the name of the virtual environment is defined in the GH Actions workflow
 venv_name <- "virtual_env"
 reticulate::use_virtualenv(file.path(getwd(), venv_name), required = TRUE)
