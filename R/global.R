d79545845a1a2e0dbe3af37a5d81368f580068f4
andrewelamb <andrewelamb@gmail.com>
2022-03-29 10:07:48 -0700

    remove print statements

--- R/global.R
+++ R/global.R
@@ -2,9 +2,6 @@
 Sys.unsetenv("RETICULATE_PYTHON")
 # Note, the name of the virtual environment is defined in the GH Actions workflow
 venv_name <- "virtual_env"
-venv_path <- file.path(getwd(), venv_name)
-print(venv_path)
-list.files(getwd())
-reticulate::use_virtualenv(venv_path, required = TRUE)
+reticulate::use_virtualenv(file.path(getwd(), venv_name), required = TRUE)
 # We get a '126' error (non-executable) if we don't do this:
 system(sprintf("chmod -R +x %s", venv_name))
