47bce0fb695cfc504340d0507f2ab65b2a43860e
andrewelamb <andrewelamb@gmail.com>
2022-03-29 11:04:47 -0700

    replace print with message

--- R/global.R
+++ R/global.R
@@ -1,8 +1,9 @@
 message("global test1")
+message(paste(list.files(), collapse = ", "))
+message("global test2")
 # Activate virtual env
 Sys.unsetenv("RETICULATE_PYTHON")
 # Note, the name of the virtual environment is defined in the GH Actions workflow
-message("global test2")
 venv_name <- "virtual_env"
 reticulate::use_virtualenv(file.path(getwd(), venv_name), required = TRUE)
 # We get a '126' error (non-executable) if we don't do this:
