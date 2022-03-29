f06569ca668bf1b6734362384156e5d9769ab397
andrewelamb <andrewelamb@gmail.com>
2022-03-29 11:17:44 -0700

    replace print with message

--- R/global.R
+++ R/global.R
@@ -1,10 +1,12 @@
 message("global test1")
-message(paste(list.files(), collapse = ", "))
-message("global test2")
 # Activate virtual env
 Sys.unsetenv("RETICULATE_PYTHON")
 # Note, the name of the virtual environment is defined in the GH Actions workflow
 venv_name <- "virtual_env"
-reticulate::use_virtualenv(file.path(getwd(), venv_name), required = TRUE)
 # We get a '126' error (non-executable) if we don't do this:
+message("global test2")
 system(sprintf("chmod -R +x %s", venv_name))
+message("global test3")
+reticulate::use_virtualenv(file.path(getwd(), venv_name), required = TRUE)
+message("global test4")
+
